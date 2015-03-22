c3a95162668ca1108eee18834323678c3e5eb870
alex.khoroshko <alex.khoroshko@gmail.com>
2015-03-23 00:34:48 +0700

    temporary files folder implementation is complete

--- sibsau_ap.py
+++ sibsau_ap.py
@@ -12,31 +12,65 @@
 #    You should have received a copy of the GNU General Public License
 #    along with sibsau-ap.  If not, see <http://www.gnu.org/licenses/>.
 
-import json, sys
+import json, sys, os
 from optparse import OptionParser
 parser = OptionParser()
-parser.add_option("-s", "--settings", dest="settings_path", help="set path to settings files folder (mandatory)")
+parser.add_option("-c", "--config", dest="settings_path", help="set path to config files folder (mandatory).")
 (options, args) = parser.parse_args()
 
 #test for path to global settings file
-try:
-    print (options.settings_path)
-except:
-    print('No settings path given. Exiting')
+if not options.settings_path:
+    print('No global config path given. Exiting.')
     sys.exit(0)
+
+print ('Trying to load global config from path: "' + options.settings_path + '"')
 
 #parsing config file  
 import os.path
 if not os.path.isdir(options.settings_path):
-    print('Specified settings folder doesn\'t exist. Exiting')
+    print('Specified config folder doesn\'t exist. Exiting.')
     sys.exit(0)
-settings_file_path = options.settings_path + 'settings.json'
+print('Specified config folder exists')
 
+settings_file_path = options.settings_path + 'sibsau_ap_cfg.json'
+print('Trying to open global config file: "' + settings_file_path + '"' )
+if not os.path.isfile(settings_file_path):
+    print('Global config file doesn\'t exist. Exiting')
+    sys.exit(0)
+print('Global config file exists' )    
 try:
     with open(settings_file_path) as settings_file:    
-        data = json.load(settings_file)
+        global_cfg = json.load(settings_file)
 except:
-    print('Failed to parse global config file. Exiting')
+    print('Failed to parse global config file with error:\n\t' + str(sys.exc_info()) + '\n\tExiting.')
     sys.exit(0)
-            
-from pprint import pprint  
+print('Global config file parsed successfully')
+
+#Check if tmp files folder exists and create if not
+try:
+    tmp_files_folder = global_cfg['tmp_files_folder']
+except:
+    print('Critical error: global config file doesn\'t have mandatory field "tmp_files_folder". Exiting.')
+    sys.exit(0)
+if os.path.isdir(tmp_files_folder):
+    print('Temporary files folder exists.')
+else:
+    print('Temporary files folder doesn\'t exist. Trying to create.')
+    try:
+        os.makedirs(tmp_files_folder)
+        print('Temporary files folder create success.')
+    except:
+        print('Critical: failed to create dir. Exiting.')
+        sys.exit(0)  
+        
+#Clean-up tmp folder
+print('Cleaning-up the tmp files folder:')
+for the_file in os.listdir(tmp_files_folder):
+    file_path = os.path.join(tmp_files_folder, the_file)
+    print('\tremoving file: "' + file_path + '"')
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except:
+        print ('Error occured while removing files:\n\t' + str(sys.exc_info()) )
+print ('\tdone.')                  
